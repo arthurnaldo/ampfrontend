1f132503bfd7b5258cddabe7a6a563edfd1cff87
Atharv Naidu <atharvan.naidu@outlook.com>
2025-02-24 14:14:20 -0800

    HELP

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
-   /** @type {import('next').NextConfig} */
-   const nextConfig = {
-    images: {
-      unoptimized: true, // Disable image optimization
-      domains: ['encrypted-tbn0.gstatic.com'], // Add your allowed image domains here
-    },
-    output: 'export', // Enable static export
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+ images: {
+   unoptimized: true, // Disable image optimization
+   domains: ['encrypted-tbn0.gstatic.com'], // Add your allowed image domains here
+ },
+ output: 'export', // Enable static export
+ basePath: '/your-repo-name', // Set this to your GitHub repo name
+};
 
-  export default nextConfig;
+export default nextConfig;
