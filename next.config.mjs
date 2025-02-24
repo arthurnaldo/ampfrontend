d0db5815fd4f57afb5724119e97a6c95fb7e5ef9
Atharv Naidu <atharvan.naidu@outlook.com>
2025-02-24 14:01:14 -0800

    build

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   images: {
     domains: ['encrypted-tbn0.gstatic.com'], // Add your allowed image domains here
   },
-  output: 'export', // Add this line to enable static export
 };
 
 export default nextConfig;
